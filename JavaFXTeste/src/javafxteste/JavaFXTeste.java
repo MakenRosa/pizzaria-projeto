8a31fd7121f01510b950ee0e0d453461e6ee765a
MakenRosa <maken.cristhian@gmail.com>
2023-04-05 16:37:04 -0300

    Tela principal semi-finalizada

--- JavaFXTeste/src/javafxteste/JavaFXTeste.java
+++ JavaFXTeste/src/javafxteste/JavaFXTeste.java
@@ -18,7 +18,7 @@ public class JavaFXTeste extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
         
         Scene scene = new Scene(root);
         
